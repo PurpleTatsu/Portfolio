0cea5ec2e5e1894b3103b64f3c82aef88c5e325d
WingedGuard <katiepgray@gmail.com>
2019-11-22 08:49:26 -0500

    redesign

---
+++ script.js
@@ -0,0 +1,22 @@
+// document.querySelector("#afghanistan").addEventListener("click", function () {
+//   console.log("afghanistan")
+// });
+
+
+
+document.querySelector("#hawaii").addEventListener("click", function () {
+  console.log("hawaii")
+});
+
+// document.querySelector("#garuda-shield").addEventListener("click", function () {
+//   console.log("garuda-shield")
+// });
+// document.querySelector("#kuwait").addEventListener("click", function () {
+//   console.log("kuwait")
+// });
+// document.querySelector("#lava").addEventListener("click", function () {
+//   console.log("lava")
+// });
+// document.querySelector("#singapore").addEventListener("click", function () {
+//   console.log("singapore")
+// }); 
